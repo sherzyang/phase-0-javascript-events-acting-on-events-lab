00b0ad4523afab16e71da1ea0ece8021aaf5b9d2
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-09 16:16:04 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0 && left <= 360) {
+      dodger.style.left = `${left - 1}px`;
+    }
+    else if (left === 0 ) {
+        dodger.style.left = `${left}px`;
+    }
+    return dodger.style.left;
+}
+
+function moveDodgerRight() {
+    const newLeftNumbers = dodger.style.left.replace("px", "");
+    const newLeft = parseInt(newLeftNumbers, 10);
+
+    if (newLeft >= 0 && newLeft < 360) {
+    dodger.style.left = `${newLeft + 1}px`;
+    }
+    else if (newLeft === 360 ) {
+        dodger.style.left = `${newLeft}px`;
+    }
+    return dodger.style.left;
+}
+
+document.addEventListener("keydown", function (e) {
+if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+}
+else if (e.key === "ArrowRight"){
+    moveDodgerRight();
+    }
+});
